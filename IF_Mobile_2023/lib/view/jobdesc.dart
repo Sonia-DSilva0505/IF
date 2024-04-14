ec7dd31a7efc877b2dee2898b8317ef7163fad48
Sonia D'Silva <sonia.dsilva.2003@gmail.com>
2024-04-14 20:34:13 +0530

    App name and icon

--- IF_Mobile_2023/lib/view/jobdesc.dart
+++ IF_Mobile_2023/lib/view/jobdesc.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:internship_fair/constants/constants.dart';
 import 'package:internship_fair/controller/apply_job.dart';
